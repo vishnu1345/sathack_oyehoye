11496458a8d3c7d00f5c59c4a5a3ce02d9e5d770
Vishnu1345 <vishnu149816@gmail.com>
2024-11-16 03:42:07 +0530

    basic

--- backend/mongo.js
+++ backend/mongo.js
@@ -24,6 +24,10 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+const ngoSchema = new mongoose.Schema({
+  
+})
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = { User };
