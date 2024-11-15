b8c22f360d0b11db5918cb7829c2e1d785ca041c
Jeevant Verma <jverma_be23@thapar.edu>
2024-11-16 03:27:46 +0530

    added cards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,16 +5,18 @@ import './App.css'
 import Dashboard from './components/Dashboard';
 import RegisterPage from './components/RegisterPage';
 import LoginPage from './components/LoginPage';
+import Cards from './components/Cards';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <Router>
+    <Router>  
       <Routes>
       <Route path="/" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/cards" element={<Cards />} />
       </Routes>
     </Router>
   )
